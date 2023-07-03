872c6f9a9eff37e71fb208d69beffb6caa37db87
ritikranjan12 <ritik123453@gmail.com>
2023-07-03 12:47:54 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://mlstfdijhecwjzufozax.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sc3RmZGlqaGVjd2p6dWZvemF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5MzEyNjUsImV4cCI6MjAwMzUwNzI2NX0.joEuomDJWsjvhpbuaEylUd-5p5hFhw9W9x6hr8wBGMY',
+    supabaseUrl: '',
+    supabaseKey: '',
   }
